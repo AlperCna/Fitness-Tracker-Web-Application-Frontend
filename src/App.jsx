d23e486fad60a67bc19a59a6ed5a06620a6d4684
AlperCna <alpercn04@gmail.com>
2025-12-03 21:55:45 +0300

    Day 8 Complete: Frontend setup with React, Vite and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+    return (
+        <div className="mt-10 text-center">
+            <h1 className="text-4xl font-bold text-red-600">
+                Tailwind Çalışıyor!
+            </h1>
+
+            <p className="text-blue-500 mt-4">
+                Bu yazı mavi olmalı.
+            </p>
+        </div>
+    );
+}
+
+export default App;
